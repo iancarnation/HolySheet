a54cffb23e2f974182a83b0b193be87a2cf341d3
Ian Rich <iancarnation@gmail.com>
2014-05-05 11:02:14 -0700

    Initial Commit
    Can currently open an image file and display it

---
+++ AIE_C#Tool/WPFexercise/WPFexercise/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFexercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "image"; // Default file name
+            dlg.DefaultExt = ".png"; // Default file extension
+            dlg.Filter = "Image FIles(*.png;*.jpg;*.gif;*.bmp)|*.png;*.jpg;*.gif;*.bmp"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // grab the file name of the image
+                string filename = dlg.FileName;
+
+                // and load the image
+
+                canvas1.LoadImage(filename);
+            }
+        }
+
+        private void MenuItemSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ AIE_C#Tool/WPFexercise/WPFexercise/MyCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WPFexercise
+{
+    public class MyCanvas : Canvas
+    {
+        BitmapImage background = null;
+
+        public void LoadImage(string filename)
+        {
+            background = new BitmapImage(new Uri(filename));
+            this.InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext dc)        {            if (background != null)            {                dc.DrawImage(background, new Rect(0, 0, background.PixelWidth, background.PixelHeight));            }        }
+    }
+}
